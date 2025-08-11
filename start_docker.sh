ead58fc640a53f6a1bb922e410168012716e3dcb
wolxXx <devops@wolxxx.de>
2025-08-11 09:00:06 +0200

    Enhance `start_docker.sh` with environment checks and `.env` handling
    Added pre-start checks for Docker, Docker Compose, and the presence of a `.env` file, with automatic creation and optional editing of `.env`. Implemented validation for essential environment variables to ensure proper configuration before starting containers. Adjusted Docker Compose commands for improved reliability.

--- start_docker.sh
+++ start_docker.sh
@@ -3,6 +3,46 @@
 here=$(dirname $(readlink -f $0))
 cd $here
 
+
+
+check_docker() {
+    if ! command -v docker &> /dev/null; then
+        echo "Error: docker is not installed"
+        exit 1
+    fi
+}
+
+check_docker_compose() {
+    if ! docker compose version &> /dev/null; then
+        echo "Error: docker compose is not installed"
+        exit 1
+    fi
+}
+
+check_env_file() {
+  if [ ! -f ".env" ]; then
+      echo ".env file is missing."
+      echo "Creating .env file from .env.dist."
+      echo "please adjust the ports and names to your needs."
+      cp .env.dist .env
+
+      read -p "Do you want to edit the .env file with vim? (y/N): " response
+      if [[ "$response" =~ ^[Yy]$ ]]; then
+          vim .env
+      else
+          exit 0
+      fi
+  fi
+}
+
+
+check_docker
+check_docker_compose
+check_env_file
+
+source .env
+
+
 echo "Starting CertWatch Docker environment..."
 
 # Check if docker-compose is available
@@ -12,11 +52,30 @@ if ! command -v "docker" &> /dev/null; then
 fi
 
 # Start docker containers
-docker compose up -d --build --force-recreate --pull always
+
 source .env
+
+
+required_vars=("COMPOSE_PROJECT_NAME" "WEB_PORT")
+missing_vars=()
+
+for var_name in "${required_vars[@]}"; do
+    if [ -z "${!var_name}" ]; then
+        missing_vars+=("$var_name")
+    fi
+done
+
+if [ ${#missing_vars[@]} -ne 0 ]; then
+    echo "Error: The following variables are not set in .env file: ${missing_vars[*]}"
+    exit 1
+fi
+
+
+docker compose --env-file .env --file docker-compose.yml  up -d --build --force-recreate --pull always
 
 # Check if containers started successfully
 if [ $? -eq 0 ]; then
+    docker compose exec web php ./composer.phar install
     echo "CertWatch Docker environment is running under http://127.0.0.1:$WEB_PORT"
 else
 
